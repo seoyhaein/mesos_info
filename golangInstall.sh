b03e73978a4e04b20f7ce082af16e7dab4855f20
seoyhaein <52847807+seoyhaein@users.noreply.github.com>
2019-11-08 20:56:49 +0900

    Update golangInstall.sh

--- golangInstall.sh
+++ golangInstall.sh
@@ -6,7 +6,7 @@ mkdir -p ${SHELL_PATH}/Arch && cd ${SHELL_PATH}/Arch
 wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
 
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-source ~/.bashrc
+~/.bashrc
 
 cat > /etc/profile.d/golang-env.sh << 'EOF'
 export GOPATH=$HOME/go
@@ -14,4 +14,4 @@ export GOBIN=$GOPATH/bin
 export PATH=$PATH:/usr/local/go/bin:$GOBIN
 EOF
 
-source /etc/profile.d/golang-env.sh
+/etc/profile.d/golang-env.sh
