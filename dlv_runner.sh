49c93b66c5cfd9e1c2e5f7ff237acb1758066ee5
seoyhaein <52847807+seoyhaein@users.noreply.github.com>
2021-03-30 19:52:45 +0900

    Update dlv_runner.sh

--- dlv_runner.sh
+++ dlv_runner.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-dlv --listen=:2345 --headless=true --api-version=2 --continue exec ./example-executor 
+dlv --listen=:2345 --headless=true --api-version=2 --accept-multiclient exec ./example-executor --continue 
+
